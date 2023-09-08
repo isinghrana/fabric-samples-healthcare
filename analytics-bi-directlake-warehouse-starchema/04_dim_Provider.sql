aec3105b0bf91df6ea23e1f5b106c3b5f1024085
gregbeaumont <64994911+gregbeaumont@users.noreply.github.com>
2023-09-08 11:31:54 -0500

    Create 04_dim_Provider.sql

---
+++ analytics-bi-directlake-warehouse-starchema/04_dim_Provider.sql
@@ -0,0 +1,20 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+CREATE VIEW [dbo].[cms_provider_dim_provider] AS (SELECT       [Prscrbr_First_Name]
+            ,(CASE WHEN [Prscrbr_Full_Name] IS NULL THEN 'missing' ELSE [Prscrbr_Full_Name] END) AS [Prscrbr_Full_Name]
+            ,[Prscrbr_Last_Org_Name]
+            ,[Prscrbr_NPI]
+            ,(CASE WHEN [Prscrbr_Type] IS NULL THEN 'missing' ELSE [Prscrbr_Type] END) AS [Prscrbr_Type] 
+            ,[Prscrbr_Type_Src]
+            ,MAX([Year]) AS [Max_Year]
+            ,MIN([Year]) AS [Min_Year]
+            ,row_number() OVER (ORDER BY [Prscrbr_NPI] ASC) AS [provider_key]
+FROM [CMS_Lakehouse].[dbo].[cms_provider_drug_costs]
+GROUP BY [Prscrbr_First_Name],[Prscrbr_Full_Name],[Prscrbr_Last_Org_Name],[Prscrbr_NPI],[Prscrbr_Type],[Prscrbr_Type_Src])
+
+GO
