7753231000d9122fcac710229a64be0dcd463527
gregbeaumont <64994911+gregbeaumont@users.noreply.github.com>
2023-09-08 12:13:33 -0500

    Update 06_cms_provider_fact_star.sql

--- analytics-bi-directlake-warehouse-starchema/06_cms_provider_fact_star.sql
+++ analytics-bi-directlake-warehouse-starchema/06_cms_provider_fact_star.sql
@@ -1,26 +1,28 @@
+-- This query is used to create a View in the Warehouse for a fact table. The View will be used to populate a table in the Lakehouse for use with a Direct Lake connection.
+
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-
-
-CREATE VIEW [dbo].[cms_provider_fact_star] AS (SELECT      [GE65_Bene_Sprsn_Flag]
-            ,[GE65_Sprsn_Flag]
-            ,[GE65_Tot_30day_Fills]
-            ,[GE65_Tot_Benes]
-            ,[GE65_Tot_Clms]
-            ,[GE65_Tot_Day_Suply]
-            ,[GE65_Tot_Drug_Cst]
-            ,[Tot_30day_Fills]
-            ,[Tot_Benes]
-            ,[Tot_Clms]
-            ,[Tot_Day_Suply]
-            ,[Tot_Drug_Cst]
-            ,[Year]
-            ,b.[drug_key]
-            ,c.[geo_key]
-            ,d.[provider_key]
+CREATE VIEW [dbo].[cms_provider_fact_star] AS 
+(
+SELECT [GE65_Bene_Sprsn_Flag]
+,[GE65_Sprsn_Flag]
+,[GE65_Tot_30day_Fills]
+,[GE65_Tot_Benes]
+,[GE65_Tot_Clms]
+,[GE65_Tot_Day_Suply]
+,[GE65_Tot_Drug_Cst]
+,[Tot_30day_Fills]
+,[Tot_Benes]
+,[Tot_Clms]
+,[Tot_Day_Suply]
+,[Tot_Drug_Cst]
+,[Year]
+,b.[drug_key]
+,c.[geo_key]
+,d.[provider_key]
 FROM [CMS_Warehouse].[dbo].[cms_provider_fact_no_null_key] a
 LEFT OUTER JOIN [CMS_Lakehouse].[dbo].[cms_provider_dim_drug] b ON a.[Brnd_Name] = b.[Brnd_Name] AND a.[Gnrc_Name] = b.[Gnrc_Name]
 LEFT OUTER JOIN [CMS_Lakehouse].[dbo].[cms_provider_dim_geography] c ON a.[Prscrbr_City_State] = c.[Prscrbr_City_State] 
