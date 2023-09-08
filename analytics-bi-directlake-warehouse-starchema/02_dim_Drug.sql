7ce93bc25b6463b40d9999b3ee5e5b3cfdce251d
gregbeaumont <64994911+gregbeaumont@users.noreply.github.com>
2023-09-08 11:28:19 -0500

    Create 02_dim_Drug.sql

---
+++ analytics-bi-directlake-warehouse-starchema/02_dim_Drug.sql
@@ -0,0 +1,16 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+CREATE VIEW [dbo].[cms_provider_dim_drug] AS (SELECT [Brnd_Name]
+            ,[Gnrc_Name]
+            ,MAX([Year]) AS [Max_Year]
+            ,MIN([Year]) AS [Min_Year]
+            ,row_number() OVER (ORDER BY [Gnrc_Name] ASC) AS [drug_key]
+FROM [CMS_Lakehouse].[dbo].[cms_provider_drug_costs]
+GROUP BY [Brnd_Name],[Gnrc_Name])
+
+GO
