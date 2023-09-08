d5c08d9d8f64bb27bd361e02c0c16f4ee5a77ee5
gregbeaumont <64994911+gregbeaumont@users.noreply.github.com>
2023-09-08 11:33:12 -0500

    Create 05_fact_no_null_key.sql

---
+++ analytics-bi-directlake-warehouse-starchema/05_fact_no_null_key.sql
@@ -0,0 +1,32 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[cms_provider_fact_no_null_key] AS (SELECT [Brnd_Name]
+            ,[GE65_Bene_Sprsn_Flag]
+            ,[GE65_Sprsn_Flag]
+            ,[GE65_Tot_30day_Fills]
+            ,[GE65_Tot_Benes]
+            ,[GE65_Tot_Clms]
+            ,[GE65_Tot_Day_Suply]
+            ,[GE65_Tot_Drug_Cst]
+            ,[Gnrc_Name]
+            ,[Prscrbr_City]
+            ,[Prscrbr_City_State]
+            ,[Prscrbr_First_Name]
+            ,(CASE WHEN [Prscrbr_Full_Name] IS NULL THEN 'missing' ELSE [Prscrbr_Full_Name] END) AS [Prscrbr_Full_Name]
+            ,[Prscrbr_Last_Org_Name]
+            ,[Prscrbr_NPI]
+            ,[Prscrbr_State_Abrvtn]
+            ,[Prscrbr_State_FIPS]
+            ,(CASE WHEN [Prscrbr_Type] IS NULL THEN 'missing' ELSE [Prscrbr_Type] END) AS [Prscrbr_Type]
+            ,[Prscrbr_Type_Src]
+            ,[Tot_30day_Fills]
+            ,[Tot_Benes]
+            ,[Tot_Clms]
+            ,[Tot_Day_Suply]
+            ,[Tot_Drug_Cst]
+            ,[Year]
+FROM [CMS_Lakehouse].[dbo].[cms_provider_drug_costs])
+GO
