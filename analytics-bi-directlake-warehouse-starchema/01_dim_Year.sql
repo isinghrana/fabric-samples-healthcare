9d4e43d179d432620c657cf1c1e1a7e0a4828892
gregbeaumont <64994911+gregbeaumont@users.noreply.github.com>
2023-09-08 11:41:35 -0500

    Update 01_dim_Year.sql

--- analytics-bi-directlake-warehouse-starchema/01_dim_Year.sql
+++ analytics-bi-directlake-warehouse-starchema/01_dim_Year.sql
@@ -1,15 +1,15 @@
+-- This query is used to create a View in the Warehouse for a Year dimension
+
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-
-
-
-
-CREATE VIEW [dbo].[cms_dim_year] AS (SELECT DISTINCT [Year]
+CREATE VIEW [dbo].[cms_dim_year] AS 
+(
+SELECT DISTINCT [Year]
 ,CAST(CONCAT('1-1-',CAST([Year] AS varchar)) AS date) AS [Year_Date_Key] 
-FROM [CMS_Lakehouse].[dbo].[cms_provider_drug_costs])
-
+FROM [CMS_Lakehouse].[dbo].[cms_provider_drug_costs]
+)
 
 GO
