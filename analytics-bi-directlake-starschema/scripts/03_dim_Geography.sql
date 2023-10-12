65a64e8495d412ff2bdb732a49bda488479af7dc
gregbeaumont <64994911+gregbeaumont@users.noreply.github.com>
2023-10-12 11:36:19 -0500

    Update 03_dim_Geography.sql

--- analytics-bi-directlake-starschema/scripts/03_dim_Geography.sql
+++ analytics-bi-directlake-starschema/scripts/03_dim_Geography.sql
@@ -4,17 +4,20 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE VIEW [dbo].[cms_provider_dim_geography] AS 
-(
-SELECT [Prscrbr_City]
-,[Prscrbr_City_State]
-,[Prscrbr_State_Abrvtn]
-,[Prscrbr_State_FIPS]
-,MAX([Year]) AS [Max_Year]
-,MIN([Year]) AS [Min_Year]
-,row_number() OVER (ORDER BY [Prscrbr_State_Abrvtn] ASC) AS [geo_key]
+
+
+
+
+
+CREATE VIEW [dbo].[cms_provider_dim_geography] AS (SELECT       [Prscrbr_City]
+            ,(CASE WHEN [Prscrbr_City_State] IS NULL THEN 'XX' ELSE [Prscrbr_City_State] END) AS [Prscrbr_City_State]
+            ,[Prscrbr_State_Abrvtn]
+            ,[Prscrbr_State_FIPS]
+            ,MAX([Year]) AS [Max_Year]
+            ,MIN([Year]) AS [Min_Year]
+            ,row_number() OVER (ORDER BY [Prscrbr_State_Abrvtn] ASC) AS [geo_key]
 FROM [CMS_Lakehouse].[dbo].[cms_provider_drug_costs]
-GROUP BY [Prscrbr_City],[Prscrbr_City_State],[Prscrbr_State_Abrvtn],[Prscrbr_State_FIPS]
-)
+GROUP BY [Prscrbr_City],[Prscrbr_City_State],[Prscrbr_State_Abrvtn],[Prscrbr_State_FIPS])
+
 
 GO
