8416d6317690663f2de2610be620f2125ffc1955
gregbeaumont <64994911+gregbeaumont@users.noreply.github.com>
2024-01-03 11:38:10 -0600

    Update 03_cms_provider_dim_geography.sql

--- analytics-bi-directlake-starschema/scripts/03_cms_provider_dim_geography.sql
+++ analytics-bi-directlake-starschema/scripts/03_cms_provider_dim_geography.sql
@@ -11,7 +11,7 @@ CREATE VIEW [dbo].[cms_provider_dim_geography] AS (SELECT       [Prscrbr_City]
             ,[Prscrbr_State_FIPS]
             ,MAX([Year]) AS [Max_Year]
             ,MIN([Year]) AS [Min_Year]
-            ,row_number() OVER (ORDER BY [Prscrbr_State_Abrvtn] ASC) AS [geo_key]
+            ,row_number() OVER (ORDER BY [Prscrbr_State_Abrvtn],[Prscrbr_City_State] ASC) AS [geo_key]
 FROM [CMS_Lakehouse].[dbo].[cms_provider_drug_costs]
 GROUP BY [Prscrbr_City],[Prscrbr_City_State],[Prscrbr_State_Abrvtn],[Prscrbr_State_FIPS])
 
