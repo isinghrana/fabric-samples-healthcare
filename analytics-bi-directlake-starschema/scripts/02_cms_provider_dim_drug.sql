97d7d22ced6fa268c9607bef853b156f32f056d5
gregbeaumont <64994911+gregbeaumont@users.noreply.github.com>
2024-11-07 17:04:25 -0600

    Delete analytics-bi-directlake-starschema/scripts/02_cms_provider_dim_drug.sql

--- analytics-bi-directlake-starschema/scripts/02_cms_provider_dim_drug.sql
+++
@@ -1,7 +0,0 @@
-SELECT [Brnd_Name]
-,[Gnrc_Name]
-,MAX([Year]) AS [Max_Year]
-,MIN([Year]) AS [Min_Year]
-,row_number() OVER (ORDER BY [Brnd_Name],[Gnrc_Name] ASC) AS [drug_key]
-FROM [CMS_Lakehouse].[dbo].[cms_provider_drug_costs]
-GROUP BY [Brnd_Name],[Gnrc_Name]
